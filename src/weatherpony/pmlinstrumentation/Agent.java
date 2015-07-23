f6fd910f382d3196eb5142858588b80be09aaf2f
The_WeatherPony <weatherpony@gmail.com>
2015-07-22 20:29:27 -0700

    Bug Fixes and Improvements
    Fixed critical issues in ASM for ClassLoader manipulations, as well as
    moved saving of changed code for later reuse.

--- src/weatherpony/pmlinstrumentation/Agent.java
+++ src/weatherpony/pmlinstrumentation/Agent.java
@@ -1,9 +1,13 @@
 package weatherpony.pmlinstrumentation;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
 
@@ -25,6 +29,13 @@ public class Agent{
 	
 	public static void agentmain(String s, Instrumentation i){
 		System.out.println("PML Agent loaded!");
+		boolean devPrerun = System.getProperty("pml.devPrerun") != null;
+		if(devPrerun){
+			System.out.println("PML Agent noticed devPrerun property. Preparing for Bootstrap class alteration...");
+			baseSaveDir = "PML/alteredBootClasses";
+		}
+		
+		wipe();
 		
 		// initialization code:
 		transformer = new ClassLoaderTransformer();
@@ -46,12 +57,48 @@ public class Agent{
 		try{
 			instrumentation.retransformClasses(change.toArray(new Class[change.size()]));
 		}catch(UnmodifiableClassException e){
+			System.err.println("PML Agent was unable to retransform classes");
 			System.exit(10);
+		}
+		if(devPrerun){
+			System.out.println("PML Agent finished devPrerun operation. Closing.");
+			System.exit(0);
 		}
 	}
 	public static void handleError(String error){
 		System.err.println(error);
 		System.exit(50);
+	}
+	static String baseSaveDir = "PML/savedAlteredClasses";
+	static void wipe(){
+		File saveRoot = new File(baseSaveDir);
+		if(saveRoot.exists()){
+			deleteFileRecursive(saveRoot);
+		}
+	}
+	static void deleteFileRecursive(File delete){
+		if(!delete.exists())
+			return;
+		if(delete.isFile())
+			delete.delete();
+		else{
+			for(File sub : delete.listFiles()){
+				sub.delete();
+			}
+		}
+	}
+	static void saveClass(String name, byte[] data){
+		try {
+			File save = new File(baseSaveDir, name+".class");
+			if(save.exists())
+				save.delete();
+			save.getParentFile().mkdirs();
+			save.createNewFile();
+			Files.write(save.toPath(), data);
+			System.out.println("saving generated replacement class bytes for "+name);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
